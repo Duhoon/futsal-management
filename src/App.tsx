10b5cf1c550740e3af70f28266e24f2d22c335ea
Duhoon <eclip6@naver.com>
2024-06-02 18:43:03 +0900

    feat: App 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+
+import Rank from './components/Register';
+import Table from './components/Table'
+import Logo from './components/Logo';
+import Board from './components/Board';
+
+import { Team } from './types/team.type';
+import { TeamsDummy, PlayerDummy } from './constants/player.dummy';
+
 import './App.scss'
 
 function App() {
+  const [teams, setTeams] = useState<Team[]>(TeamsDummy);
+  const header = ['번호', '이름']
 
   return (
-    <></>
+    <>
+      <Logo/>
+      <Rank></Rank>
+      <Board>
+        {teams.map((team, index)=>(
+          <Table index={index + 1} header={header} rows={team.player} />
+        ))}
+      </Board>
+      
+    </>
   )
 }
 
