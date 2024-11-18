d1acb3e43f34158b3ba689195998f9d53bb20a8d
Duhoon <eclip6@naver.com>
2024-11-19 01:16:57 +0900

    feature: scss config setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import { useState } from 'react';
-
-import Rank from './components/Register';
-import Table from './components/Table'
-import Logo from './components/Logo';
-import Board from './components/Board';
-
-import { Team } from './types/team.type';
-import { TeamsDummy, PlayerDummy } from './constants/player.dummy';
-
-import './App.scss'
+import { Header } from './components';
 
 function App() {
-  const [teams, setTeams] = useState<Team[]>(TeamsDummy);
-  const header = ['번호', '이름']
-
   return (
     <>
-      <Logo/>
-      <Rank></Rank>
-      <Board>
-        {teams.map((team, index)=>(
-          <Table index={index + 1} header={header} rows={team.player} />
-        ))}
-      </Board>
-      
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/header.module.scss';
+
+export default function Header() {
+    return (
+        <header className={styles["logo"]}>
+            풋살 전술 보드
+        </header>
+    )
+}
