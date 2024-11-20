a0ea11cea4942c0ebfa562d74d229613eec2535e
Duhoon <eclip6@naver.com>
2024-11-21 03:40:41 +0900

    feature: Header menu button 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 import styles from "@/styles/header.module.scss";
+import { FaBars } from "react-icons/fa6";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.logo}>풋살 전술 보드</h1>
+      <h1 className={styles.logo}>Futsal Board</h1>
+      <div className={styles["menu-bar"]}>
+        <button className={styles["menu-button"]}>
+          <FaBars fill={"#fff"} className={styles["menu-icon"]} />
+        </button>
+      </div>
     </header>
   );
 }
--- src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import "./logo.scss";
-
-export default function Logo() {
-  return (
-    <div className="logo-wrapper">
-      <img className="logo-image" src="/team-logo.PNG" />
-    </div>
-  );
-}
